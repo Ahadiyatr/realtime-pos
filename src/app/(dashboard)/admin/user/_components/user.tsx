ca4c0bd4120652b8b7e8c97cd830ef47d17c0115
Ahadiyat Rakhman <ahadiyat.rakhman@gmail.com>
2025-08-27 20:52:38 +0800

    Datatable UI

--- src/app/(dashboard)/admin/user/_components/user.tsx
+++ src/app/(dashboard)/admin/user/_components/user.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { DataTable } from "@/components/common/data-table";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { HEADER_TABLE_USER } from "@/constants/user-constant";
 import { createClient } from "@/lib/supabase/client";
 import { Dialog, DialogTrigger } from "@radix-ui/react-dialog";
 import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 import { toast } from "sonner";
 
 export default function UserManagement() {
@@ -25,6 +28,12 @@ export default function UserManagement() {
       return data;
     },
   });
+
+  const filteredData = useMemo(() => {
+    return (users || []).map((user, index) => {
+      return [index + 1, user.id, user.name, user.role, ""];
+    });
+  }, [users]);
   return (
     <div className="w-full">
       <div className="flex flex-col lg:flex-row mb-4 gap-2 justify-between w-full">
@@ -38,12 +47,11 @@ export default function UserManagement() {
           </Dialog>
         </div>
       </div>
-      {users?.map(user => (
-        <div key={user.id}>
-          <h2>{user.name}</h2>
-          <h2>{user.role}</h2>
-        </div>
-      ))}
+      <DataTable
+        header={HEADER_TABLE_USER}
+        data={filteredData}
+        isLoading={isLoading}
+      />
     </div>
   );
 }
