47f0057b9d4bc370f545fb2d4f5aabed67b545d5
Ahadiyat Rakhman <ahadiyat.rakhman@gmail.com>
2025-09-11 02:14:10 +0800

    Update User & Delete User

--- src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
+++ src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
@@ -1,37 +1,22 @@
-import FormInput from '@/components/common/form-input';
-import { Button } from '@/components/ui/button';
-import {
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog';
-import { Form } from '@/components/ui/form';
 import {
   INITIAL_CREATE_USER_FORM,
   INITIAL_STATE_CREATE_USER,
-  ROLE_LIST,
 } from '@/constants/auth-constant';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Loader2 } from 'lucide-react';
 import { startTransition, useActionState, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import {
-  createUserForm,
+  CreateUserForm,
   createUserSchema,
 } from '@/validations/auth-validation';
-import { createUser } from '../action';
-import FormSelect from '@/components/common/form-select';
-import FormImage from '@/components/common/form-image';
+import { createUser } from '../actions';
 import { Preview } from '@/types/general';
 import FormUser from './form-user';
 
 export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
-  const form = useForm<createUserForm>({
+  const form = useForm<CreateUserForm>({
     resolver: zodResolver(createUserSchema),
     defaultValues: INITIAL_CREATE_USER_FORM,
   });
--- src/validations/auth-validation.ts
+++ src/validations/auth-validation.ts
@@ -16,5 +16,15 @@ export const createUserSchema = z.object({
   ]),
 });
 
+export const updateUserSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  role: z.string().min(1, 'Role is required'),
+  avatar_url: z.union([
+    z.string().min(1, 'Image URL is Required'),
+    z.instanceof(File),
+  ]),
+});
+
 export type LoginForm = z.Infer<typeof loginSchemaForm>;
-export type createUserForm = z.Infer<typeof createUserSchema>;
+export type CreateUserForm = z.Infer<typeof createUserSchema>;
+export type UpdateUserSchema = z.Infer<typeof updateUserSchema>;
