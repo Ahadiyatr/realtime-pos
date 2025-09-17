c0321cdd6cff9c82b620c5a39d708032265edeff
Ahadiyat Rakhman <ahadiyat.rakhman@gmail.com>
2025-09-18 02:39:36 +0800

    Add To Cart

--- src/app/(dashboard)/order/[id]/add/_components/add-order-item.tsx
+++ src/app/(dashboard)/order/[id]/add/_components/add-order-item.tsx
@@ -4,14 +4,17 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { FILTER_MENU } from '@/constants/order-constant';
 import useDataTable from '@/hooks/use-data-table';
+import { createClient } from '@/lib/supabase/client';
 import { useQuery } from '@tanstack/react-query';
 import { toast } from 'sonner';
-import LoadingCardMenu from './loading-card-menu';
 import CardMenu from './card-menu';
-import { createClient } from '@/lib/supabase/client';
+import LoadingCardMenu from './loading-card-menu';
 import CartSection from './cart';
+import { useState } from 'react';
+import { Cart } from '@/types/order';
+import { Menu } from '@/validations/menu-validation';
 
-export default function AddorderItem({ id }: { id: string }) {
+export default function AddOrderItem({ id }: { id: string }) {
   const supabase = createClient();
   const {
     currentSearch,
@@ -19,6 +22,7 @@ export default function AddorderItem({ id }: { id: string }) {
     handleChangeSearch,
     handleChangeFilter,
   } = useDataTable();
+
   const { data: menus, isLoading: isLoadingMenu } = useQuery({
     queryKey: ['menus', currentSearch, currentFilter],
     queryFn: async () => {
@@ -63,6 +67,48 @@ export default function AddorderItem({ id }: { id: string }) {
     enabled: !!id,
   });
 
+  const [carts, setCarts] = useState<Cart[]>([]);
+
+  const handleAddToCart = (menu: Menu, action: 'increment' | 'decrement') => {
+    const existingItem = carts.find(item => item.menu_id === menu.id);
+    if (existingItem) {
+      if (action === 'decrement') {
+        if (existingItem.quantity > 1) {
+          setCarts(
+            carts.map(item =>
+              item.menu_id === menu.id
+                ? {
+                    ...item,
+                    quantity: item.quantity - 1,
+                    total: item.total - menu.price,
+                  }
+                : item
+            )
+          );
+        } else {
+          setCarts(carts.filter(item => item.menu_id !== menu.id));
+        }
+      } else {
+        setCarts(
+          carts.map(item =>
+            item.menu_id === menu.id
+              ? {
+                  ...item,
+                  quantity: item.quantity + 1,
+                  total: item.total + menu.price,
+                }
+              : item
+          )
+        );
+      }
+    } else {
+      setCarts([
+        ...carts,
+        { menu_id: menu.id, quantity: 1, total: menu.price, notes: '', menu },
+      ]);
+    }
+  };
+
   return (
     <div className="flex flex-col lg:flex-row gap-4 w-full">
       <div className="space-y-4 lg:w-2/3">
@@ -91,7 +137,11 @@ export default function AddorderItem({ id }: { id: string }) {
         ) : (
           <div className="grid grid-cols-2 lg:grid-cols-3 w-full gap-4">
             {menus?.data?.map(menu => (
-              <CardMenu menu={menu} key={`menu-${menu.id}`} />
+              <CardMenu
+                menu={menu}
+                key={`menu-${menu.id}`}
+                onAddToCart={handleAddToCart}
+              />
             ))}
           </div>
         )}
@@ -100,7 +150,12 @@ export default function AddorderItem({ id }: { id: string }) {
         )}
       </div>
       <div className="lg:w-1/3">
-        <CartSection order={order} />
+        <CartSection
+          order={order}
+          carts={carts}
+          setCarts={setCarts}
+          onAddToCart={handleAddToCart}
+        />
       </div>
     </div>
   );
